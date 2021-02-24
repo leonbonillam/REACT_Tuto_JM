af2768e129447d8b72ffaff776f2271fa30fd87e
leonMarquez <leonbonillam@gmail.com>
2021-02-24 16:13:38 -0600

    Eventos nativos, sinteticos y personalizados

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
-import Eventos from "./components/Eventos";
+import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
 
 import "./App.css";
 
@@ -70,7 +70,11 @@ function App() {
             <hr />
             <RenderizadoElementos />
             <hr />
-            <Eventos />
+            <EventosES6 />
+            <hr />
+            <EventosES7 />
+            <hr />
+            <MasSobreEventos />
             <hr />
           </section>
         </header>
--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Eventos extends Component {
+export class EventosES6 extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -33,12 +33,96 @@ export default class Eventos extends Component {
   render() {
     return (
       <div>
-        <h2>Eventos en Componentes de Clases</h2>
+        <h2>Eventos en Componentes de Clases ES6</h2>
         <nav>
           <button onClick={this.sumar}>+</button>
           <button onClick={this.restar}>-</button>
         </nav>
         <h3>{this.state.contador}</h3>
+      </div>
+    );
+  }
+}
+//Propierties Initializer
+export class EventosES7 extends Component {
+  state = {
+    contador: 0,
+  };
+  //ARROW FUNCTIONS
+
+  sumar = (e) => {
+    console.log("Sumando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  };
+
+  restar = (e) => {
+    console.log("Reastando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Eventos en Componentes de Clases ES7</h2>
+        <nav>
+          <button onClick={this.sumar}>+</button>
+          <button onClick={this.restar}>-</button>
+        </nav>
+        <h3>{this.state.contador}</h3>
+      </div>
+    );
+  }
+}
+
+/* function Boton(props) {
+  return <button onClick={props.MyOnClick}>Botón hecho componente</button>;
+} */
+
+/* const Boton = ({ props }) => {
+  <button onClick={props.myOnClick}>Botón hecho componente</button>;
+}; */
+
+const Boton = ({ myOnClick }) => (
+  <button onClick={myOnClick}>Botón hecho componente</button>
+);
+export class MasSobreEventos extends Component {
+  handleClick = (e, mensaje) => {
+    console.log(e);
+    console.log(e.nativeEvent);
+    console.log(e.nativeEvent.target);
+    console.log(e.target);
+    console.log(mensaje);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Más sobre eventos</h2>
+        <button
+          onClick={(e) =>
+            this.handleClick(e, "Parametro pasado desde un evento")
+          }
+        >
+          Saludar
+        </button>
+        {/* Evento personalizado */}
+        {/* <Boton
+          onClick={(e) =>
+            this.handleClick(e, "Parametro pasado desde un evento")
+          }
+        /> */}
+
+        <Boton
+          myOnClick={(e) =>
+            this.handleClick(e, "Parametro pasado desde un evento")
+          }
+        />
       </div>
     );
   }
